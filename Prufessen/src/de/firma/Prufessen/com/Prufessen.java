b0bef900830ae53aeb6420c1a1403a263b1f0f42
Elvis <mbahforsab@yahoo.com>
2017-10-02 18:02:20 +0200

    Thetet

--- Prufessen/src/de/firma/Prufessen/com/Prufessen.java
+++ Prufessen/src/de/firma/Prufessen/com/Prufessen.java
@@ -4,7 +4,7 @@ public class Prufessen {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello world cup  thetetrr");
+System.out.println("Hello world cup  thetetrrggggggg");
 	}
 
 }
