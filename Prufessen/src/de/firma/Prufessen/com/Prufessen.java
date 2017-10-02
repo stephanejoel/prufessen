e22c3726b436da73b6281ecd4ee86d9c3c53cd75
stephane joel <stephanejoel1986@yahoo.fr>
2017-10-02 16:46:54 +0200

    repos2

---
+++ Prufessen/src/de/firma/Prufessen/com/Prufessen.java
@@ -0,0 +1,10 @@
+package de.firma.Prufessen.com;
+
+public class Prufessen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello world");
+	}
+
+}
